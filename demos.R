cc6126bbbaa39d2e06dea521f362b3910ca8f7e8
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-09-21 15:50:36 -0400

    rounddf() demo

--- demos.R
+++ demos.R
@@ -1,6 +1,5 @@
 
 # interpm
-
 source('interpm.R')
 
 dat <- data.frame(time = 1:30, a = rnorm(30), b = rnorm(30), c = rnorm(30))
@@ -10,3 +9,13 @@ dat[20:22, 'a'] <- NA
 dat
 dat2 <- interpm(dat, 'time', c('a', 'b', 'c'))
 dat2
+
+# rounddf
+source('rounddf.R')
+
+dat <- data.frame(a = 1:10, b = rnorm(10), c = letters[1:10])
+
+rounddf(dat)
+rounddf(dat, digits = c(0, 4))
+rounddf(dat, digits = c(0, 4), func = signif)
+
