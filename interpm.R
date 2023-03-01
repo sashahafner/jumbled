31800e749916699cb0bc66e1726ca1a9318fe955
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-01 13:12:39 -0500

    Add by argument to interpm.

--- interpm.R
+++ interpm.R
@@ -1,12 +1,25 @@
 # Replace missing values in multiple data frame columns with interpolated values
 
-interpm <- function(dat, x, ys, ...) {
+interpm <- function(dat, x, ys, by = NA, ...) {
 
-  for (i in ys) {
-    rout <- which(is.na(dat[[i]])) 
-    if (length(rout) > 0) {
-      dat[[i]][rout] <- approx(dat[[x]][-rout], dat[[i]][-rout], xout = dat[[x]][rout], ...)$y 
-    } 
+  if (is.na(by)) {
+    for (i in ys) {
+      rout <- which(is.na(dat[[i]])) 
+      if (length(rout) > 0) {
+        dat[[i]][rout] <- approx(dat[[x]][-rout], dat[[i]][-rout], xout = dat[[x]][rout], ...)$y 
+      } 
+    }
+  } else {
+    for (i in ys) {
+      for (j in unique(dat[[by]])) {
+        gr <- dat[[by]] == j
+        rout <- is.na(dat[[i]])
+        if (length(rout) > 0) {
+          dat[[i]][gr & rout] <- approx(dat[[x]][gr & !rout], dat[[i]][gr & !rout], xout = dat[[x]][gr & rout], ...)$y 
+        } 
+      }
+    }
+
   }
 
   return(dat)
