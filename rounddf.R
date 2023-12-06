7b0e628e392ea3e9a46b8ed7e752fa76fd7f889f
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-12-06 13:56:58 -0500

    Add pad arg to rounddf

--- rounddf.R
+++ rounddf.R
@@ -1,5 +1,5 @@
 
-rounddf <- function(x, digits = rep(2, ncol(x)), func = round) {
+rounddf <- function(x, digits = rep(2, ncol(x)), func = round, pad = FALSE) {
 
   # Convert data.table to data frame
   DT <- FALSE
@@ -10,6 +10,7 @@ rounddf <- function(x, digits = rep(2, ncol(x)), func = round) {
     }
   }
 
+  # Get digits vector if needed
   if (length(digits) == 1) {
     digits <- rep(digits, ncol(x))
   } else if (length(digits) != ncol(x)) {
@@ -17,12 +18,23 @@ rounddf <- function(x, digits = rep(2, ncol(x)), func = round) {
     warning('First value in digits repeated to match length.')
   }
 
-  for(i in 1:ncol(x)) {
-    if(class(x[, i, drop = TRUE])[1] == 'numeric') x[, i] <- func(x[, i], digits[i])
+  # Round
+  for (i in 1:ncol(x)) {
+    if (class(x[, i, drop = TRUE])[1] == 'numeric') {
+      x[, i] <- func(x[, i], digits[i])
+      # Pad with trailing zeroes (only works well in some cases)
+      # Ignored if every row does not have decimal point
+      if (pad && all(grepl('\\.', x[, i]))) {
+        ff <- max(nchar(gsub('.+\\.', '', x[, i])))
+        fmt <- paste0('%.0', ff, 'f')
+        x[, i] <- sprintf(fmt, x[, i])
+      }
+    }
   }
 
+  # Convert back to data.table if that was input format
   if (DT) {
-    x <- data.table(x)
+    x <- data.table::data.table(x)
   }
 
   return(x)
